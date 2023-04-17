f4c77a89d8e16afaad01f85e97c5dae1c92e0269
ngntrgduc <trungducnguyen1511@gmail.com>
2023-04-18 00:50:32 +0700

    Update code

--- week3.R
+++ week3.R
@@ -3,26 +3,27 @@
 # "q" + tên phân phối: quatile : phân vị mức q
 # "r" + tên phân phối: tạo random sample
 
+# các phân phối: binom, chisq, exp, norm, pois, t, unif
+
 ?rnorm
-dunif()
 punif(seq(0, 1, by=0.25))
 runif(100)
 
 x <- seq(-5, 5, by=0.1)
 x
-plot(x, dnorm(x, 0, 1), col="blue", type="l", lwq=2, lty=2) # lty là line type, lwd là line width
+plot(x, dnorm(x, 0, 1))
+plot(x, dnorm(x, 0, 1), col="blue", type="l", lwd=2, lty=2) # lty là line type, lwd là line width
 ?abline
 abline(v=0, col="red", lty=2) # thêm dòng vô cho plot
 
 z <- function() runif(1)
 z()
 
+?runif
 # Mô phỏng đồng xu
 throw.coin <- function() {
-  x <- runif(1)
-  if (x <= 1/2) 0 else 1
+  if (runif(1) <= 1/2) 0 else 1
 }
-
 throw.coin()
 
 # Tung đồng xu n lần
@@ -35,7 +36,6 @@ table(x)
 # Tần suất
 table(x) / length(x)
 
-?runif
 # Mô phỏng xúc xắc
 throw.dice <- function() {
   x <- runif(1)
@@ -46,7 +46,7 @@ throw.dice <- function() {
 throw.dice()
 
 # Các phân phối xác suất thông dụng
-## Nếu X ~ N(0, 1) thì X^2 ~ ChiSquare(1)
+## Nếu X ~ N(0, 1) thì X^2 ~ Chi^2(1)
 n <- 10000
 x <- rnorm(n)
 y <- x^2
@@ -55,6 +55,7 @@ curve(dchisq(x, df=1), col="red", add = TRUE)
 ?curve
 ?hist
 
+## Nếu X ~ Chi^2(m), Y ~ Chi^2(n) thì X + Y ~ Chi^2(m + n)
 m1 <- 10
 m2 <- 20
 sample.x <- rchisq(n, m1)
@@ -62,10 +63,14 @@ sample.y <- rchisq(n, m2)
 hist(sample.x + sample.y, freq=0, breaks=40)
 curve(dchisq(x, df=m1+m2), col="red", add=TRUE)
 
+# Khoảng ước lượng các biến ngẫu nhiên liên tục
+# Xét khoảng dạng [a, b]
+# P(X <= a) = P(X >= b) = (1 - p) / 2, 0 < p < 1
+# P(X <= b) = 1 - P(X >= b) = (1 + p) / 2
 mu <- 10
 sigma <- 4
 p <- 0.9
 a <- qnorm((1-p)/2, mean=mu, sd=sigma)
 b <- qnorm((1+p)/2, mean=mu, sd=sigma)
-khoang <- c(a, b)
-khoang
+interval.p <- c(a, b)
+interval.p
--- week4.R
+++ week4.R
@@ -17,12 +17,9 @@ test <- function(n) {
 	return(p.propotion)
 }
 
-p.test1 <- test(100)
-p.test1
-p.test2 <- test(1000)
-p.test2
-p.test3 <- test(10000)
-p.test3
+p.test1 <- test(100); p.test1
+p.test2 <- test(1000); p.test2
+p.test3 <- test(10000); p.test3
 
 # n.vec <- c(10, 100, 1000, 10000, 100000)
 n.vec <- 10^(seq(1, 5, by=1))
@@ -35,7 +32,7 @@ for (i in 1:length(n.vec)) {
 }
 plot(n.vec, p.testvec, type='l', col='blue', lwd=2, xlab="n", ylab="p")
 lines(n.vec, rep(p, length(n.vec)), col='red', lty=4, lwd=2)
-
+?lines
 # X ~ Student(m)
 # X ~ ChiSquare(m)
 # X ~ Fisher
@@ -49,7 +46,8 @@ mean.sample <- function(n) {
 n <- 1000
 m <- 1000
 mean.sample.vec <- replicate(m, mean.sample(n))
-hist(mean.sample.vec, seq=0, breaks=40)
+hist(mean.sample.vec, freq=0, breaks=40)
+?hist
 curve(dnorm(x, mu, sigma/sqrt(n)), col="red", add=TRUE)
 
 n <- 10
@@ -61,5 +59,5 @@ var.sample <- function() {
 
 m <- 10000
 var.sample.vec <- replicate(m, var.sample())
-hist(var.sample.vec, freq=0, breaks=0, col="lightblue")
+hist(var.sample.vec, freq=0, breaks=40, col="lightblue")
 curve(dchisq(x, df=n-1), col="red", add=TRUE)
